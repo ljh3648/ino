93f47affb7e3dc070c48e6c2f04aafe2d2106a7e
ljh3648 <ljh09060@gmail.com>
2022-06-02 02:13:00 +0900

    Update 반응속도게임.ino
    전반적으로 변수와 함수의 이름 규칙성을 통일하려고 했고
    설명과 보기 편하도록 코드를 수정

--- 반응속도게임.ino
+++ 반응속도게임.ino
@@ -10,31 +10,27 @@
 #define B 494 // 시 
 
 // LED 디지털핀 지정
-//#define D1 8
-//#define D2 9
-//#define D3 10
 int led_D[3] = { 8,9,10 };
 
 // SW 디지털핀 지정
-//#define SW1 5
-//#define SW2 6
-//#define SW3 7
 int sw_D[3] = { 5,6,7 };
 
+// 피에조 디지털핀 지정
 #define BUZ 4
 
 // 게임 소리
-int game_start[] = { C,D,E,F,G,F,E,D,C };
-int game_value[10] = { C,D,E,F,G,A,B,C,D,E };
-int game_end[] = { F,F,F,F,F,F,F,F };
-int game_clear[] = { G, G, A, A, G, G, E, G, G, E, E, D, G, G, A, A, G, G, E, G, E, D, E, C };
+int game_start[] = { C,D,E,F,G,F,E,D,C }; // 게임 시작 시 소리
+int game_value[10] = { C,D,E,F,G,A,B,C,D,E }; // 게임 난이도 표시 소리
+int game_end[] = { F,F,F,F,F,F,F,F }; // 게임 패배 소리
+int game_clear[] = { G, G, A, A, G, G, E, G, G, E, E, D, G, G, A, A, G, G, E, G, E, D, E, C }; // 게임 클리어 소리 "학교종일땡땡땡"
+
 // 단계 난이도
-int level_DFCLT[10] = { 2000,1500,1000,800,700,600,500,400,400,400};
+int level_DFCLT[10] = { 2000,1500,1000,800,700,600,500,400,300,150};
 
 void setup() {
-    // put your setup code here, to run once:
     digitalWrite(12,HIGH);
     Serial.begin(9600);
+    
     for (int i = 0; i < 3; i++) {
         pinMode(led_D[i], OUTPUT);
         pinMode(sw_D[i], INPUT_PULLUP);
@@ -50,30 +46,40 @@ void loop() {
     int r = 0; // 랜덤 배정 확인
     bool game_ruling = true; //게임 승패 확인 승리 true, 패배 false
 
-    GAME_SOUND(1, 0); // 게임시작 소리 내기
-    LED_ON_ALL();
+    GAME_SOUND(1, 0); // 게임시작 소리
+    LED_ON_ALL();     // LED 3초간 점등후 소등
+    
 
-    for (int i=1; i<=10; i++) {
-        gameloop(i);
+    for (int level=1; level<=10; level++) { //i는 난이도 1단계부터 10단계 까지.
+        GAMELOOP(level);
     }
-    GAME_SOUND(4,0);
+    
+    GAME_SOUND(4,0); // 게임클리어 소리
 }
 
-int LED_ON_R(void) {
-    int r = rand() % 3;
-    digitalWrite(led_D[r], HIGH);
-    return r;
+// 게임진행되는 곳
+void GAMELOOP(int level) {
+    int r = GAME_Q_RAND(); // 랜덤배정 받는 변수
+    bool game_ruling = true; //게임 승패 확인 승리 true, 패배 false
+
+    GAME_SOUND(2, level); // 1단계 시작 음
+    LED_ON_R(r);
+    game_ruling = GAME_READING(r, level_DFCLT[level]);// 게임 판정 보내기
+    if (game_ruling == false) {
+        LED_OFF_ALL();
+        GAME_SOUND(3, 0); // 종료 브금
+        GAMEOVER();
+    }
+    LED_OFF_ALL();
+    delay(1000);
 }
 
+// 게임 승패판정
 bool GAME_READING(int r, int g_time) {
     bool game_ruling = false;
     unsigned int time_previous = millis();
     unsigned int time_current = millis();
     while (time_current - time_previous <= g_time) {
-        Serial.println("While문 진입");
-        Serial.println(digitalRead(sw_D[0]));
-        Serial.println(digitalRead(sw_D[1]));
-        Serial.println(digitalRead(sw_D[2]));
         if (digitalRead(sw_D[r]) == LOW) { //LED 켜진 위치와 눌린 스위치가 같다면
             return game_ruling = true; // 성공
         }
@@ -82,6 +88,16 @@ bool GAME_READING(int r, int g_time) {
     }
 
     return game_ruling = false; // 패배
+}
+
+int GAME_Q_RAND(void){ // 게임 문제 랜덤 생성
+  int r = rand()%3;
+  return r;
+}
+
+// LED 제어 함수
+void LED_ON_R(int r) { // 랜덤배정 받은 LED를 켜는곳
+    digitalWrite(led_D[r], HIGH);
 }
 
 void LED_ON_ALL() { // LED 3초간 점등후 소등
@@ -98,6 +114,8 @@ void LED_OFF_ALL() {
     }
 }
 
+// 게임의 사운드 관리 함수
+// song은 음악 트랙 선택, cnt는 게임 단계에 맞는 소리의 갯수를 가져옴.
 void GAME_SOUND(int song, int cnt) {
     switch (song) {
     case 1:
@@ -127,26 +145,7 @@ void GAME_SOUND(int song, int cnt) {
     }
 }
 
-void gameloop(int level) {
-    int r = 0; // 랜덤 배정 확인
-    bool game_ruling = true; //게임 승패 확인 승리 true, 패배 false
-
-    GAME_SOUND(2, level); // 1단계 시작 음
-    r = LED_ON_R();
-    game_ruling = GAME_READING(r, level_DFCLT[level]);// 스위치 입력 받기
-    if (game_ruling == false) {
-        LED_OFF_ALL();
-        GAME_SOUND(3, 0); // 종료 브금
-        Serial.println("실패");
-        gameover();
-    }
-    else if (game_ruling == true) {
-        Serial.println("성공!");
-    }
-    LED_OFF_ALL();
-    delay(1000);
-}
-
-void gameover() {
+// 게임 리셋 초기화
+void GAMEOVER() {
     digitalWrite(12, LOW);
 }
